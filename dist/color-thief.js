33872bdb60e19df681da0b188e646db168e34039
Tommy McMillan <tommymcmillan@Tommys-MacBook-Pro.local>
2023-12-16 09:12:32 +0100

    updated package name and repo url

--- dist/color-thief.js
+++ dist/color-thief.js
@@ -28,10 +28,10 @@ function validateOptions(options) {
     if (typeof colorCount === 'undefined' || !Number.isInteger(colorCount)) {
         colorCount = 10;
     } else if (colorCount === 1 ) {
-        throw new Error('colorCount should be between 2 and 50. To get one color, call getColor() instead of getPalette()');
+        throw new Error('colorCount should be between 2 and 20. To get one color, call getColor() instead of getPalette()');
     } else {
         colorCount = Math.max(colorCount, 2);
-        colorCount = Math.min(colorCount, 50);
+        colorCount = Math.min(colorCount, 20);
     }
 
     if (typeof quality === 'undefined' || !Number.isInteger(quality) || quality < 1) {
